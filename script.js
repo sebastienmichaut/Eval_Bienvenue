75aa0632e8ed80057b52f011bef8fc85790ebdd6
Sébastien Michaut <sebmichaut00@gmail.com>
2021-04-07 17:31:44 +0200

    last commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var caroussel = document.getElementsByClassName("imgveh1");
+moveLeft = document.getElementById("moveleft")
+moveRight = document.getElementById("moveright")
+
+caroussel.addEventListener("click", function () {
+  return 
+});
