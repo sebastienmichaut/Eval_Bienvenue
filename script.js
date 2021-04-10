4364428230dc82b98c422f70898e123a08c59f9c
Sébastien Michaut <sebmichaut00@gmail.com>
2021-04-10 18:50:20 +0200

    java

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-var caroussel = document.getElementsByClassName("imgveh1");
-moveLeft = document.getElementById("moveleft")
-moveRight = document.getElementById("moveright")
+// ------------ image du header -----------
 
-caroussel.addEventListener("click", function () {
-  return 
-});
+var i = 0;
+var images = [
+  "/images/background.jpg",
+  "/images/background1.jpg",
+  "/images/background2.jpg",
+  "/images/background3.jpg",
+  "/images/background4.jpg",
+];
+var time = 7000;
+
+function changeImg() {
+  document.imageHeader.src = images[i];
+    if (i < images.length - 1) {
+      i++;
+    }  else {
+      i=0;
+    }
+    setTimeout("changeImg()", time);
+}
+window.onload = changeImg;
+
+// -------------- carrousel ----------------
+
+var imageVehicule = [
+  "/images.vehicule1.png",
+  "/images.vehicule2.png",
+  "/images.vehicule3.png",
+  "/images.vehicule4.png",
+];
+
+// let prevButton = document.getElementsByName('prev');
+// let nextButton = document.getElementsByName('next');
+
+// nextButton.addEventListener("click",function () {
+//   console.log('clic')
+// })
